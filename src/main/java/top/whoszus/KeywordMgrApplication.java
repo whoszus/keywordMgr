5facc52d1ac677bcf35d1dec388a38846b81024b
whoszus <whoszus@yahoo.com>
2017-03-03 22:09:38 +0800

    Initial commit

---
+++ src/main/java/top/whoszus/KeywordMgrApplication.java
@@ -0,0 +1,12 @@
+package top.whoszus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeywordMgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeywordMgrApplication.class, args);
+	}
+}
---
+++ src/test/java/top/whoszus/KeywordMgrApplicationTests.java
@@ -0,0 +1,16 @@
+package top.whoszus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KeywordMgrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
